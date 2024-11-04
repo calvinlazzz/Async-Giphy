f7114e1bea9bb07ea107c3ad02a2e252a55cab93
Calvin La <calvin.la@clover.com>
2024-11-04 15:03:18 -0800

    attempt 2 to return url of the last object in data

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImage(query)
+{
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    //console.log(endpoint)
+   const response = await fetch(endpoint)
+   const data = await response.json()
+   //console.log(data)
+    const firstImage = data.data[24].url
+   return firstImage
+
+//    return fetch(endpoint)
+//    .then((response) => response.json())
+//    .then((data) => {
+//        console.log(data);
+       
+//    })
+//    .catch((error) => console.log(error));
+    
+} 
+//console.log(getImage('dog')) 
+
+getImage('dog').then(image => console.log(image)).catch(error => console.error(error));
+//console.log(process.env.API_KEY)
